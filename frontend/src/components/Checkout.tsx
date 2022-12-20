4a6a22bd4412e25a2377e10a9cb9f6ac5c8206c1
kubasn <sosinkuba@gmail.com>
2022-12-20 19:11:01 +0100

    20.12 - checkout update & add home page animation

--- frontend/src/components/Checkout.tsx
+++ frontend/src/components/Checkout.tsx
@@ -17,6 +17,15 @@ const Checkout = () => {
   const cart = useTypedSelector((state) => state.cart);
   const user = useTypedSelector((state) => state.user);
   const [items, setItems] = useState<cartType[]>([]);
+  let delivery = 10;
+  const reduce = () => {
+    let total = 0;
+    if (cart.items != null)
+      for (let val of cart.items) {
+        total += parseFloat(val.price) * val.qty;
+      }
+    return total;
+  };
 
   useEffect(() => {
     if (cart && cart.items !== null) setItems(cart.items);
@@ -27,24 +36,24 @@ const Checkout = () => {
       <div className="flex mx-auto">
         <p className=" text-stone-800 text-3xl uppercase">Checkout</p>
       </div>
-      <div className="flex justify-between ">
+      <div className="flex flex-col md:flex-row  justify-between ">
         {/* items */}
-        <div className="w-1/2 ">
+        <div className="md:w-1/2 w-[95%] mx-auto  ">
           {items.map((item) => (
-            <div className="flex gap-2 items-center relative">
+            <div className="flex gap-2 border-b-[1px] last-of-type:border-none border-stone-400 pb-4 items-center relative ">
               <img className="w-20" src={item.imageUrl} />
               <div>
                 <p className="">{item.title}</p>
                 <p className="text-[12px] text-stone-400">Qty: {item.qty}</p>
               </div>
-              <p className="absolute right-0 bottom-0 text-stone-600">
+              <p className="absolute right-2 bottom-2 text-stone-600">
                 {item.price} $
               </p>
             </div>
           ))}
         </div>
         {/* order summary */}
-        <div className=" bg-white w-1/3">
+        <div className=" bg-white mx-auto w-4/5 md:w-1/3">
           <div className="bg-gray-200 py-2 pl-2 uppercase font-semibold">
             <p>Order summary</p>
           </div>
@@ -52,15 +61,15 @@ const Checkout = () => {
           <div className="pl-2">
             <p className="flex justify-between">
               <span>Subtotal</span>
-              <span className="mr-4">560 $</span>
+              <span className="mr-4">{reduce()}</span>
             </p>
             <p className="flex justify-between">
               <span>Delivery</span>
-              <span className="mr-4">560 $</span>
+              <span className="mr-4">{delivery} $</span>
             </p>
             <p className="flex justify-between mt-4 font-semibold ">
               <span>Estimated total</span>
-              <span className="mr-4">560 $</span>
+              <span className="mr-4">{delivery + reduce()} $</span>
             </p>
           </div>
           <div className="w-4/5 h-[1px] bg-gray-200 my-4 m-auto"></div>
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Delivery from "../images/delivery.png";
 import HomeImg from "../images/homeImg.svg";
+import { motion } from "framer-motion";
 
 const Home = () => {
+  const visible = { opacity: 1, y: 0, transition: { duration: 1.0 } };
+  const visible1 = { opacity: 1, x: 0, transition: { duration: 1.0 } };
+
+  const container = {
+    hidden: { opacity: 0, y: 10 },
+    visible,
+  };
+
+  const container1 = {
+    hidden: { opacity: 0, x: -10 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1.0 } },
+  };
+
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 w-full" id="home">
       <div className="py-4 flex flex-1 flex-col items-center md:items-start justify-center gap-4 ">
@@ -14,18 +28,28 @@ const Home = () => {
             alt="delivery"
           />
         </div>
-        <p className="text-[2.4rem] lg:text-[4rem] font-bold tracking-wider text-center md:text-start text-gray-700">
+        <motion.p
+          initial="hidden"
+          animate="visible"
+          variants={container}
+          className="text-[2.4rem] lg:text-[4rem] font-bold tracking-wider text-center md:text-start text-gray-700"
+        >
           The best plants & tools for your{" "}
           <span className="text-green-600 text-[2.6rem] lg:text-[4.2rem]">
             {" "}
             GARDEN{" "}
           </span>
-        </p>
-        <p className="text-base text-gray-700 text-center md:text-left md:w-[80%]">
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          animate="visible"
+          variants={container1}
+          className="text-base text-gray-700 text-center md:text-left md:w-[80%]"
+        >
           We help realize your dreams about wonderful garden. Let's start with
           small things that can change the world, so you can enjoy the fresh air
           forever.
-        </p>
+        </motion.p>
 
         <button className="bg-green-700 text-white tracking-wide  rounded-md py-2 w-full md:w-auto px-2 hover:shadow-lg transition-all ease-in-out duration-100">
           Order Now
